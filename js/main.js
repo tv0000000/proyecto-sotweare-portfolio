918a919ea32f8b6811ec8fda09a6b84904c81914
unknown <tomasvannelli@gmail.com>
2023-07-21 11:21:20 -0300

    proyecto softweareIA portfolio terminado

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {
+  let header = document.getElementById("header");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
